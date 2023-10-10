c0e102f00c4394e0f8680a76e765aeec174a7162
Chris Whitehead <chris@varleysoftware.co.uk>
2023-10-10 13:01:31 +0100

    Fixed table updating logic. Still requires some work on select bindings (on new object). Added new attributes to recipe steps.

--- Data.Models/Models/RecipeStep.cs
+++ Data.Models/Models/RecipeStep.cs
@@ -13,5 +13,15 @@
         public string Description { get; set; } = string.Empty;
 
         public string ImgUrl { get; set; } = string.Empty;
+
+
+        // When we create a cooking recipe step, keep track of timings
+        public bool HasTimer { get; set; }
+
+        public int TimerDuration { get; set; }
+
+        public DateTime? TimerStarted { get; set; }
+
+        public DateTime? TimerEnded { get; set; }
     }
 }
